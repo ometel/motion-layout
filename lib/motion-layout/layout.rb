fdd9e7f07f66a6392a29d4c825131acb733bded3
orm <omer.metel@gmail.com>
2013-11-02 00:12:23 -0400

    Added ability to override default "options" parameter to NSLayoutConstraint.constraintsWithVisualFormat.  Each call to "horizontal" and "vertical" method may now specify one or more options describing the attribute and the direction of layout for all objects in the visual format string (each options corresponds to an NSLayoutFormatOptions).

--- lib/motion-layout/layout.rb
+++ lib/motion-layout/layout.rb
@@ -1,9 +1,10 @@
 module Motion
   class Layout
+
     def initialize(&block)
-      @verticals   = []
-      @horizontals = []
-      @metrics     = {}
+      @verticals = {}
+      @horizontals = {}
+      @metrics = {}
 
       yield self
       strain
@@ -21,12 +22,16 @@ module Motion
       @view = view
     end
 
-    def horizontal(horizontal)
-      @horizontals << horizontal
+    def horizontal(format, *options)
+      options = [:center_y] if options.empty?
+      resolved_options = resolve_options(options)
+      @horizontals[resolved_options] = (@horizontals[resolved_options] || []) << format
     end
 
-    def vertical(vertical)
-      @verticals << vertical
+    def vertical(format, *options)
+      options = [:center_x] if options.empty?
+      resolved_options = resolve_options(options)
+      @verticals[resolved_options] = (@verticals[resolved_options] || []) << format
     end
 
     private
@@ -34,20 +39,56 @@ module Motion
     def strain
       @subviews.values.each do |subview|
         subview.translatesAutoresizingMaskIntoConstraints = false
-        @view.addSubview(subview) unless subview.superview
+        @view.addSubview(subview)
       end
-
-      views = @subviews.merge("superview" => @view)
 
       constraints = []
-      constraints += @verticals.map do |vertical|
-        NSLayoutConstraint.constraintsWithVisualFormat("V:#{vertical}", options:NSLayoutFormatAlignAllCenterX, metrics:@metrics, views:views)
+
+      @verticals.each do |options, formats|
+        constraints += formats.map do |format|
+          NSLayoutConstraint.constraintsWithVisualFormat("V:#{format}", 
+            options:options || NSLayoutFormatAlignAllCenterX, 
+            metrics:@metrics, 
+            views:@subviews
+            )
+        end
       end
-      constraints += @horizontals.map do |horizontal|
-        NSLayoutConstraint.constraintsWithVisualFormat("H:#{horizontal}", options:NSLayoutFormatAlignAllCenterY, metrics:@metrics, views:views)
+
+      @horizontals.each do |options, formats|
+        constraints += formats.map do |format|
+          NSLayoutConstraint.constraintsWithVisualFormat("H:#{format}", 
+            options:options || NSLayoutFormatAlignAllCenterY, 
+            metrics:@metrics, 
+            views:@subviews
+            )
+        end
       end
 
       @view.addConstraints(constraints.flatten)
+    end
+
+    def resolve_options(options)
+      option_hash = {
+        none: 0,
+        left: NSLayoutFormatAlignAllLeft,
+        right: NSLayoutFormatAlignAllRight,
+        top: NSLayoutFormatAlignAllTop,
+        bottom: NSLayoutFormatAlignAllBottom,
+        leading: NSLayoutFormatAlignAllLeading,
+        trailing: NSLayoutFormatAlignAllTrailing,
+        center_x: NSLayoutFormatAlignAllCenterX,
+        center_y: NSLayoutFormatAlignAllCenterY,
+        baseline: NSLayoutFormatAlignAllBaseline
+      }
+      options.inject(0) do |combined_result, option|
+        if option.kind_of?(Numeric)
+          combined_result | option.to_i
+        elsif constant = option_hash[option.to_s.downcase.to_sym]
+          combined_result | constant
+        else
+          raise "invalid option: #{option.to_s.downcase}"
+        end
+      end
     end
   end
 end
